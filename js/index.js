0cf86ed546d472dc3b39526bdbd7cdfcad007aa2
stephancloete <stephancloete@gmail.com>
2022-07-28 16:13:36 +0200

    update
    Added solutions for following:
    2. Remove user by index number
    3. Display all users concatenated in DOM

--- js/index.js
+++ js/index.js
@@ -9,27 +9,39 @@ function userChoice(){
     switch(input){
         //Add three users
         case '1':
-        userOne = prompt("Add new user #1:");
-        userTwo = prompt("Add new user #2");
-        userThree = prompt("Add new user #3");
+            userOne = prompt("Add new user #1:");
+            userTwo = prompt("Add new user #2");
+            userThree = prompt("Add new user #3");
 
-        userList.push(userOne, userTwo, userThree);
-        console.log(userList);
-        
-        const newUsers = []
+            userList.push(userOne, userTwo, userThree);
+            console.log(userList);
+            
+            const newUsers = []
 
             break;
 
         //Remove users
         case '2':
-      
+            toRemove = prompt("Type the index number of the user you would like to remove:");
+            var removed = userList.splice(toRemove, 1);
+            console.log(removed);
+            console.log(userList);
             break;
 
         //Display all users
         case '3':
-           
-            break;
+            let result = '';
+            
+            for (let i = 0; i < userList.length; i++) {
+            result = result.concat(userList[i]);
+            }
+            
+            console.log(result);
 
+            document.getElementById("join").innerHTML = result;
+            //console.log(joined);*/
+            break;
+3
         //Sort users
         case '4':
          
